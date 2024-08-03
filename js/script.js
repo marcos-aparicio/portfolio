51718ec33c2bcdf6f502c2736a08f97fa1f3f3c2
marcos-aparicio <marcos.aparicio593@gmail.com>
2024-08-03 15:44:04 -0500

    fixing icon bug

--- js/script.js
+++ js/script.js
@@ -46,6 +46,7 @@ const selectedTheme = localStorage.getItem("theme");
 if (selectedTheme) {
   document.body.classList.add(selectedTheme);
 } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+  localStorage.setItem("theme", "theme-dark");
   document.body.classList.add("theme-dark");
 } else {
   localStorage.setItem("theme", "theme-light");
