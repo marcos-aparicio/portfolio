a36fdb84863eb50e3d1cbfc5d7d1992bf846ae6e
marcos-aparicio <marcos.aparicio593@gmail.com>
2024-09-29 12:39:40 -0400

    finishing props component section

--- js/sections/props.js
+++ js/sections/props.js
@@ -1,5 +1,28 @@
 const COMPONENT_NAME = "props-section";
 
+const props = [
+  {
+    idea: "How to implement a theme switcher",
+    source:
+      "https://www.youtube.com/watch?v=vIBKSmWAdIA&ab_channel=TomIsLoading",
+  },
+  {
+    idea: "Sliding Text Animation from first section",
+    source: "https://codepen.io/chenxinnn/pen/qGKPOE",
+  },
+  {
+    idea: "AlpineJS Carousel Component",
+    source: "https://www.penguinui.com/components/carousel",
+  },
+  {
+    idea: "Inspiration to use WebComponents",
+    source: "https://www.freecodecamp.org/news/build-your-first-web-component/",
+  },
+  {
+    idea: "Idea to use simple throttle function",
+    source: "https://www.sitepoint.com/throttle-scroll-events/",
+  },
+];
 class Props extends HTMLElement {
   constructor() {
     super();
@@ -7,21 +30,48 @@ class Props extends HTMLElement {
 
   connectedCallback() {
     this.innerHTML = `
-      <section id="props" class="min-h-svh py-8">
+      <section id="props" class="min-h-svh py-8 flex flex-col gap-8">
         <p class="text-8xl font-bold">Props</p>
         <p class="text-2xl">
           I like to recognize when I am not the original author of an idea, for
           this website I <strong>definitely</strong> used some code from other
-          people, I don't think they'll ever see this but I want to acknowledge
-          how my website was inspired from that code.
+          people or was inspired by their code. I don't think they'll ever see this but I want to acknowledge them.
         </p>
 
-        <ul>
-          <li>example 1</li>
-          <li>example 2</li>
-        </ul>
-
-        <p>Built with 🧡 using AlpineJS, Neovim, Tailwind and Vite</p>
+        <ol
+          x-data="{
+          props: [
+              {
+                idea: 'How to implement a theme switcher',
+                source:
+                  'https://www.youtube.com/watch?v=vIBKSmWAdIA&ab_channel=TomIsLoading',
+              },
+              {
+                idea: 'Sliding Text Animation from first section',
+                source: 'https://codepen.io/chenxinnn/pen/qGKPOE',
+              },
+              {
+                idea: 'AlpineJS Carousel Component',
+                source: 'https://www.penguinui.com/components/carousel',
+              },
+              {
+                idea: 'Inspiration to use WebComponents',
+                source: 'https://www.freecodecamp.org/news/build-your-first-web-component/',
+              },
+              {
+                idea: 'Idea to use simple throttle function',
+                source: 'https://www.sitepoint.com/throttle-scroll-events/',
+              },
+            ]
+        }"
+          class="text-secondary flex gap-8 flex-col list-decimal list-inside"
+        >
+        <template x-for="prop in props">
+          <li>
+            <a :href="prop.source" x-text="prop.idea" class="underline-effect" target="_blank"></a>
+          </li>
+        </template>
+        </ol>
       </section>
     `;
   }
