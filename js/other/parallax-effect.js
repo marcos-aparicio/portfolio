ad399909e13c2e062d6032d776c78f92a3b5631b
marcos-aparicio <marcos.aparicio593@gmail.com>
2024-10-01 17:41:17 -0400

    adapting parallax js and index.html to avoid bugs regarding the parallax effect

--- js/other/parallax-effect.js
+++ js/other/parallax-effect.js
@@ -1,8 +1,19 @@
 import { throttle } from "../helpers";
+function getOrientation() {
+  return window.innerWidth > window.innerHeight ? "landscape" : "portrait";
+}
+// Example usage
+const orientation = getOrientation();
+// only allow parallax effect on landscape orientation
 function parallaxEffect(scrollPos) {
   // container that holds the image to produce the parallax effect
   let parallax = document.getElementById("parallax");
-  parallax.style.backgroundPositionY = `-${scrollPos * 0.2}px`;
+  let parallaxMovement =
+    orientation === "landscape"
+      ? 0.2 * scrollPos
+      : 0.05 * scrollPos * 0.1 * 0.1;
+
+  parallax.style.backgroundPositionY = `-${parallaxMovement}px`;
 }
 
 const main = document.querySelector("main");
