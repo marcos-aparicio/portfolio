8f310469e167a1ed3545ae48db7ac36c7a0af84e
marcos-aparicio <marcos.aparicio593@gmail.com>
2024-10-01 18:05:38 -0400

    making contacts section responsive

--- js/sections/contact.js
+++ js/sections/contact.js
@@ -33,23 +33,23 @@ class Contact extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
   <section id="contact" class="min-h-svh flex flex-col gap-8 py-8">
-  <p class="text-center text-8xl font-bold">Contact</p>
-  <p class="text-center text-3xl">
+  <p class="text-6xl landscape-mobile:text-4xl xl:text-8xl text-center font-bold">Contact</p>
+  <p class="text-center text-xl md:text-2xl xl:text-3xl">
   Greetings, suggestions or anything you want. Let's get to work 🧑‍💻
   </p>
 
       <div
-        class="mx-auto flex w-fit flex-col justify-center gap-8 text-2xl"
+        class="mx-auto flex w-full xl:w-fit flex-col justify-center gap-8 landscape-mobile:flex-row flex-wrap landscape-mobile:gap-2"
         x-data="socialMediaLinks"
         >
         <template x-for="(link,i) in socialMediaLinks" :key="i">
             <a
               :href="link.url"
-              class="underline-effect flex items-center justify-between gap-16 text-3xl p-1 px-8 hover:text-primary rounded-xl"
+              class="underline-effect flex items-center justify-between w-full gap-2 md:gap-8 xl:gap-16 text-xl md:text-3xl p-1 md:px-8 hover:text-primary rounded-xl landscape-mobile:gap-2 landscape-mobile:w-fit landscape-mobile:text-lg"
               target="_blank"
             >
-              <span x-text="link.name" :class="i % 2 == 0 ? 'order-last' : ''"></span>
-              <i :class="link.icon" class="text-6xl"></i>
+              <span x-text="link.name" :class="i % 2 == 0 ? 'order-last' : ''" class="truncate ..."></span>
+              <i :class="link.icon" class="text-4xl xl:text-6xl"></i>
             </a>
         </template>
       </div>
